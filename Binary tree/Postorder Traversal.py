94e19f2b7e4b24c2727ba427d57d636fd61bf97a
Sk Mehphug Rahaman <skmehfooz03@gmail.com>
2025-11-07 13:24:14 +0530

    Add files via upload

---
+++ Binary tree/Postorder Traversal.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+# Creating tree nodes
+drink = Node("Drinks")
+hot = Node("hot")
+cold = Node("cold")
+tea = Node("tea")
+coffee = Node("coffee")
+cola = Node("cola")
+fanta = Node("fanta")
+
+# Building the binary tree structure
+hot.left = tea
+hot.right = coffee
+cold.left = cola
+cold.right = fanta
+drink.left = hot
+drink.right = cold
+
+
+def Inorder(node):#[left->right->root]
+    if node == None:
+        return
+    Inorder(node.left)
+    Inorder(node.right)
+    print(node.val)
+
+Inorder(drink)
+
+
+'''
+        Drinks
+         /   \
+       hot    cold
+      /  \    /   \
+    tea coffee cola fanta
+'''
